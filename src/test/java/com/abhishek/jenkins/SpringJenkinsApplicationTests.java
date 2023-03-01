939bb55971192aeaf3cd5d6b4cddee0066406015
Abhishek <Abhishek.Kumar2.Contractor@pepsico.com>
2023-03-01 20:40:32 +0530

    second commit

--- src/test/java/com/abhishek/jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/abhishek/jenkins/SpringJenkinsApplicationTests.java
@@ -14,6 +14,7 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Testcase is executing...");
+		logger.info("Testcase is executing...second log statement");
 		Assertions.assertEquals(true, true);
 	}
 

3cdf450e24588d61389f112ac757b68079fae8d9
Abhishek <Abhishek.Kumar2.Contractor@pepsico.com>
2023-03-01 20:11:49 +0530

    jenkins integration

---
+++ src/test/java/com/abhishek/jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,20 @@
+package com.abhishek.jenkins;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Testcase is executing...");
+		Assertions.assertEquals(true, true);
+	}
+
+}
